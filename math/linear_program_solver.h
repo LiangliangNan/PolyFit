fccf8026e39a260fcf9741072f1079539dbe27b9
Liangliang Nan <15526536+LiangliangNan@users.noreply.github.com>
2018-04-11 13:21:07 +0200

    unified interface for Gurobi, SCIP, GLPK, and LPSOLVE solvers

--- math/linear_program_solver.h
+++ math/linear_program_solver.h
@@ -1,4 +1,24 @@
-#pragma once
+/*
+Copyright (C) 2017  Liangliang Nan
+http://web.siat.ac.cn/~liangliang/ - liangliang.nan@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef _MATH_LINEAR_PROGRAM_SOLVER_H_
+#define _MATH_LINEAR_PROGRAM_SOLVER_H_
 
 #include "math_common.h"
 #include "linear_program.h"
@@ -44,3 +64,4 @@ private:
 	double				objective_value_;
 };
 
+#endif
