fbcb2d87123a76e52a76980031c0ef4397e3078a
Liangliang Nan <liangliang.nan@gmail.com>
2019-01-17 19:58:53 +0100

    added glew source code

--- renderer/glew.c
+++ renderer/glew.c
@@ -50,9 +50,9 @@
 #  if defined(NOGDI)
 #    undef NOGDI
 #  endif
-#  include <GL/wglew.h>
+#  include "wglew.h"
 #elif !defined(__ANDROID__) && !defined(__native_client__) && !defined(__HAIKU__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
-#  include <GL/glxew.h>
+#  include "glxew.h"
 #endif
 
 #include <stddef.h>  /* For size_t */
