4ac6e2a79bbdd14ec5726011dac4a7deb531a06c
Liangliang Nan <liangliang.nan@gmail.com>
2019-11-27 09:15:28 +0100

    Update opengl_info.h

--- renderer/opengl_info.h
+++ renderer/opengl_info.h
@@ -23,6 +23,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "renderer_common.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 #include <GL/glew.h>
 #include <string>
 
